e816081ad4ad3e98b66e2e8b4ade18a157d1ae3e
Peter Haraszti <peter.haraszti@protonmail.com>
2026-06-06 08:10:44 +0200

    Update xlstm_individual.py with code from POC

--- xlstm/xlstm_individual.py
+++ xlstm/xlstm_individual.py
@@ -1,21 +1,20 @@
 import gc
-import pickle
-import os
-import random
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import pickle
+import sys
+import random
+import os
+
 from datetime import datetime
 
-import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 from util.metrics import *
 from util.paths import *
 from util.experiment_params import *
 from util.sequencing import create_sequences
-
-from codecarbon import EmissionsTracker
 
 import torch
 import torch.nn as nn
@@ -25,6 +24,8 @@ from train import train_model
 
 from ray import tune
 from ray.tune.schedulers import ASHAScheduler
+
+from codecarbon import EmissionsTracker
 
 SEED = 12019844
 os.environ["PYTHONHASHSEED"] = str(SEED)
@@ -40,11 +41,8 @@ torch.backends.cudnn.benchmark = False
 
 experiment_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-# Global model params
 MODEL = "xLSTM"
 BATCH_SIZE = 24
-EARLY_STOPPING_PATIENCE = 3
-EPOCHS = 100
 
 MODELS_DIR = os.path.join("xlstm", "models")
 os.makedirs(MODELS_DIR, exist_ok=True)
@@ -53,15 +51,6 @@ os.makedirs(RESULTS_DIR, exist_ok=True)
 RESULTS_FILENAME = f"{MODEL}_results_{experiment_timestamp}.csv"
 RESULTS_FILEPATH = os.path.join(RESULTS_DIR, RESULTS_FILENAME)
 results = []
-
-def cleanup_torch():
-    gc.collect()
-
-    if torch.cuda.is_available():
-        torch.cuda.synchronize()
-        torch.cuda.empty_cache()
-        torch.cuda.ipc_collect()
-        torch.cuda.reset_peak_memory_stats()
 
 with open(SPRING_LIST_FILE, 'r') as f:
     spring_ids = [line.strip() for line in f if line.strip()]
@@ -72,8 +61,6 @@ spring_ids = ["395038"]
 for spring_id in spring_ids:
     print(f"Running {MODEL} for {spring_id}...")
 
-    cleanup_torch()
-
     SPRING_DIR = os.path.join(SPRINGS_BASE_DIR, spring_id)
     TRAIN_PATH = os.path.join(SPRING_DIR, f"{spring_id}_train.csv")
     VALID_PATH = os.path.join(SPRING_DIR, f"{spring_id}_valid.csv")
@@ -81,37 +68,37 @@ for spring_id in spring_ids:
     SCALER_Y_PATH = os.path.join(SPRING_DIR, f"{spring_id}_scale_y.pkl")
 
     if not os.path.exists(TRAIN_PATH) or not os.path.exists(VALID_PATH) or not os.path.exists(TEST_PATH) or not os.path.exists(SCALER_Y_PATH):
-        print(f"    Skipping {spring_id} because of missing data")
-        continue
+        print(f"Skipping {spring_id} because of missing data")
+        sys.exit()
 
     SPRING_MODEL_DIR = os.path.join(MODELS_DIR, spring_id)
     os.makedirs(SPRING_MODEL_DIR, exist_ok=True)
 
-    print("     Reading data...")
+    print("Preparing data...")
+
     train_df = pd.read_csv(TRAIN_PATH, parse_dates=['timestamp'])
     valid_df = pd.read_csv(VALID_PATH, parse_dates=['timestamp'])
     test_df = pd.read_csv(TEST_PATH, parse_dates=['timestamp'])
 
-    print("     Creating sequences...")
     input_cols = [c for c in test_df.columns if c not in ['timestamp']]
     X_train, y_train, ts_train  = create_sequences(train_df[input_cols], 
-                                       train_df[TARGET_COL],
-                                       train_df["timestamp"], 
-                                       WINDOW_LEN, 
-                                       FORECAST_HS)
+                                        train_df[TARGET_COL],
+                                        train_df["timestamp"], 
+                                        WINDOW_LEN, 
+                                        FORECAST_HS)
 
     X_valid, y_valid, ts_valid  = create_sequences(valid_df[input_cols], 
-                                       valid_df[TARGET_COL],
-                                       valid_df["timestamp"], 
-                                       WINDOW_LEN, 
-                                       FORECAST_HS)
+                                        valid_df[TARGET_COL],
+                                        valid_df["timestamp"], 
+                                        WINDOW_LEN, 
+                                        FORECAST_HS)
 
     X_test, y_test, ts_test  = create_sequences(test_df[input_cols], 
-                                       test_df[TARGET_COL],
-                                       test_df["timestamp"], 
-                                       WINDOW_LEN, 
-                                       FORECAST_HS)
-    
+                                        test_df[TARGET_COL],
+                                        test_df["timestamp"], 
+                                        WINDOW_LEN, 
+                                        FORECAST_HS)
+
     X_train = torch.tensor(X_train, dtype=torch.float32)
     y_train = torch.tensor(y_train, dtype=torch.float32)
 
@@ -133,110 +120,139 @@ for spring_id in spring_ids:
         shuffle=False
     )
 
-    def train_xlstm_tune(config, train_loader=None, valid_loader=None, device=None):
-        criterion = nn.L1Loss()
+    test_loader = DataLoader(
+        TensorDataset(X_test, y_test),
+        batch_size=BATCH_SIZE,
+        shuffle=False
+    )
 
-        model = xLSTMForecaster(
-            input_size=len(input_cols),
-            hidden_size=config["embedding_dim"],
-            output_size=len(FORECAST_DAYS),
-            dropout=config["dropout"],
-            dense_layers=1,
-            architecture=config["architecture"]
-        ).to(device)
+    print("Tuning...")
 
-        optimizer = torch.optim.Adam(
-            model.parameters(),
-            lr=config["lr"]
-        )
-
-        best_val_loss = train_model(
-            model=model,
-            train_loader=train_loader,
-            valid_loader=valid_loader,
-            criterion=criterion,
-            optimizer=optimizer,
-            device=device,
-            epochs=config["epochs"],
-            patience=3
-        )
-
-        tune.report(val_loss=best_val_loss)
-
-    print("     Training...")
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
-    search_space = {
+    config = {
         "embedding_dim": tune.grid_search([64, 96, 128]),
-        "dropout": tune.grid_search([0.1, 0.2]),
-        "lr": tune.grid_search([1e-2, 1e-3, 1e-4]),
+        "dropout": 0.1,
+        "lr": tune.grid_search([1e-3, 1e-4]),
         "architecture": tune.grid_search([
             "slstm_first",
             "slstm_second",
             "only_slstm",
             "only_mlstm"
-        ]),
-        "epochs": 100
+        ]),    
+        "epochs": 100,
+        "patience": 3,
+        "input_size": len(input_cols),
+        "output_size": len(FORECAST_DAYS)
     }
 
+    def cleanup_torch():
+        gc.collect()
+
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()
+            torch.cuda.empty_cache()
+            torch.cuda.ipc_collect()
+            torch.cuda.reset_peak_memory_stats()
+
+    def train_xlstm(config, X_t=None, y_t=None, X_v=None, y_v=None):
+
+        print("CUDA available:", torch.cuda.is_available())
+        print("Device count:", torch.cuda.device_count())
+        print("Current device:", torch.cuda.current_device())
+        print("GPU name:", torch.cuda.get_device_name(0))
+        print("CUDA_VISIBLE_DEVICES:", os.environ.get("CUDA_VISIBLE_DEVICES"))
+
+        device = torch.device("cuda:0")
+
+        # 2. Build the DataLoaders LOCALLY inside the worker
+        train_loader = DataLoader(
+            TensorDataset(X_t, y_t),
+            batch_size=BATCH_SIZE, 
+            shuffle=True
+        )
+        
+        valid_loader = DataLoader(
+            TensorDataset(X_v, y_v),
+            batch_size=BATCH_SIZE,
+            shuffle=False
+        )
+
+        model = xLSTMForecaster(
+            input_size=config["input_size"],
+            hidden_size=config["embedding_dim"],
+            output_size=config["output_size"],
+            dropout=config["dropout"],
+            architecture=config["architecture"]
+        ).to(device)
+
+        criterion = nn.L1Loss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"])
+
+        train_model(
+            model=model,
+            train_loader=train_loader, # Pass the locally built loaders
+            valid_loader=valid_loader,
+            criterion=criterion,
+            optimizer=optimizer,
+            device=device,
+            epochs=config["epochs"],
+            patience=config["patience"],
+            use_ray=True,
+            verbose=False
+        )
+
+
     scheduler = ASHAScheduler(
-        metric="val_loss",
-        mode="min",
-        max_t=100,
-        grace_period=10,
-        reduction_factor=3
+        max_t=config["epochs"],
+        grace_period=config["patience"],
+        reduction_factor=2,
     )
 
-    trainable = tune.with_parameters(
-        train_xlstm_tune,
-        train_loader=train_loader,
-        valid_loader=valid_loader,
-        device=device
+    trainable = tune.with_resources(
+        tune.with_parameters(
+            train_xlstm,
+            X_t=X_train,
+            y_t=y_train,
+            X_v=X_valid,
+            y_v=y_valid,
+        ),
+        resources={"cpu": 4, "gpu": 1}
     )
 
     tuner = tune.Tuner(
         trainable,
         tune_config=tune.TuneConfig(
+            metric="val_loss",
+            mode="min",
             scheduler=scheduler,
-            num_samples=72
+            max_concurrent_trials=1,  # important for debugging
         ),
-        param_space=search_space
+        param_space=config,
     )
-
-    # torch.autograd.set_detect_anomaly(True)
-
-    hyperparam_results = tuner.fit()
-
-    best_result = hyperparam_results.get_best_result(
-        metric="val_loss",
-        mode="min"
-    )
+    tuning_results = tuner.fit()
+    best_result = tuning_results.get_best_result(metric="val_loss", mode="min")
     best_config = best_result.config
     print(best_config)
 
+    cleanup_torch()
+
+    print("Training...")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     tracker = EmissionsTracker(log_level="error")
     tracker.start()
-    
+
     model = xLSTMForecaster(
-        input_size=len(input_cols),
+        input_size=best_config["input_size"],
         hidden_size=best_config["embedding_dim"],
-        output_size=len(FORECAST_DAYS),
+        output_size=best_config["output_size"],
         dropout=best_config["dropout"],
-        dense_layers=1,
         architecture=best_config["architecture"]
     ).to(device)
 
     criterion = nn.L1Loss()
-    optimizer = torch.optim.Adam(
-        model.parameters(),
-        lr=best_config["lr"]
-    )
-
-    MODEL_PATH = os.path.join(
-        SPRING_MODEL_DIR,
-        f"{MODEL}_{spring_id}_{experiment_timestamp}.pt"
-    )
+    optimizer = torch.optim.Adam(model.parameters(), lr=best_config["lr"])
+    MODEL_PATH = os.path.join(SPRING_MODEL_DIR, f"{MODEL}_{spring_id}_{experiment_timestamp}.pt")
 
     train_model(
         model=model,
@@ -245,50 +261,35 @@ for spring_id in spring_ids:
         criterion=criterion,
         optimizer=optimizer,
         device=device,
-        epochs=EPOCHS,
-        patience=EARLY_STOPPING_PATIENCE,
+        epochs=config["epochs"],
+        patience=config["patience"],
         model_save_path=MODEL_PATH
     )
 
     emissions_train = tracker.stop()
     energy_kwh_train = tracker.final_emissions_data.energy_consumed
+    
+    print("Inference...")
 
-    del train_loader, valid_loader
-    del X_train, X_valid, y_train, y_valid
-    del optimizer
-    del criterion
-    cleanup_torch()
+    model.load_state_dict(torch.load(MODEL_PATH))
+    model.eval()
 
-    print("     Inference...")
     tracker = EmissionsTracker(log_level="error")
     tracker.start()
 
-
-    model.eval()   
-
-    test_loader = DataLoader(
-        TensorDataset(X_test, y_test),
-        batch_size=8,
-        shuffle=False
-    )
-
     preds = []
-
     with torch.no_grad():
         for xb, _ in test_loader:
             xb = xb.to(device)
             preds.append(model(xb).cpu())
-
     y_pred = torch.cat(preds).numpy()
 
     emissions_inference = tracker.stop()
     energy_kwh_inference = tracker.final_emissions_data.energy_consumed
 
-
+    # Inverse transform to original scale and convert to m^3/s
     with open(SCALER_Y_PATH, 'rb') as f:
         y_scaler = pickle.load(f)
-
-    # Inverse transform to original scale and convert to m^3/s
     y_test_orig = y_scaler.inverse_transform(y_test) * 0.001
     y_pred_orig = y_scaler.inverse_transform(y_pred)  * 0.001
 
@@ -336,8 +337,4 @@ for spring_id in spring_ids:
     results_df = pd.DataFrame(results)
     results_df.to_csv(RESULTS_FILEPATH, index=False)
 
-    del model
-    del X_test
-    del y_test
-    del y_pred
     cleanup_torch()
