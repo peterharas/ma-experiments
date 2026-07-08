a7611343aa77307a8f742aa54686c6af1909d5ab
Peter Haraszti <peter.haraszti@tuwien.ac.at>
2026-07-08 12:40:10 +0200

    Add create_sequences_full_horizon_future

--- util/sequencing.py
+++ util/sequencing.py
@@ -37,3 +37,26 @@ def create_sequences_full_horizon(X, y, timestamps, window_len, max_horizon):
         ts.append(timestamps.iloc[i + window_len : i + window_len + max_horizon].values)
 
     return np.array(Xs), np.array(ys), np.array(ts)
+
+
+# expected input for future_known_input_cols: ["temperature","sh","rr","tl"]
+def create_sequences_full_horizon_future(X, y, timestamps, window_len, max_horizon, future_known_input_cols):
+    Xs, ys, future_inputs, ts = [], [], [], []
+
+    for i in range(len(X) - window_len - max_horizon + 1):
+        X_window = X.iloc[i:i + window_len].values
+        # Grab the entire sequence from t+1 to t+96
+        targets = y.iloc[i + window_len : i + window_len + max_horizon].values
+
+        # Grab the future known inputs over the prediction horizon
+        future_X = X[future_known_input_cols].iloc[i + window_len : i + window_len + max_horizon].values
+
+        if np.isnan(X_window).any() or np.isnan(targets).any() or np.isnan(future_X).any():
+            continue
+
+        Xs.append(X_window)
+        ys.append(targets)
+        future_inputs.append(future_X)
+        ts.append(timestamps.iloc[i + window_len : i + window_len + max_horizon].values)
+
+    return np.array(Xs), np.array(ys), np.array(future_inputs), np.array(ts)
